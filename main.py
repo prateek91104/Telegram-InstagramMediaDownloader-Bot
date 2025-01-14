fc185aace7632c0b575c862ec7e323e9429fde4c
prateek91104 <prateekshirol1718@gmail.com>
2025-01-15 00:51:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def send_dp(update, context):
 
 
 def main():
-    BOT_TOKEN = ''
+    BOT_TOKEN = '8011181060:AAGmdMIig9vTcN-PlVmzfjgftZsUZGAVqcI'
     updater = Updater(BOT_TOKEN, use_context=True)
     dp = updater.dispatcher
 
